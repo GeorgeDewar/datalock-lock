bb7e6b5d2d8c7df387dd9ebd0848345b53447994
George Dewar <george@dewar.co.nz>
2014-07-25 20:59:11 +1200

    Use keypad

--- lock/lock.ino
+++ lock/lock.ino
@@ -1,9 +1,31 @@
 #include <Wire.h>
 #include "rgb_lcd.h"
+#include <Keypad.h>
 
+const byte ROWS = 4; //four rows
+const byte COLS = 3; //three columns
+char keys[ROWS][COLS] = {
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
+};
+byte rowPins[ROWS] = {12, 11, 10, 9}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {8, 7, 6}; //connect to the column pinouts of the keypad
+
+/*
+ * Set up libraries
+ */
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 rgb_lcd lcd;
 
+/*
+ * Begin program
+ */
+
 void setup() {
+    Serial.begin(9600);
+  
     // set up the LCD's number of columns and rows:
     lcd.begin(16, 2);
     // Print a message to the LCD.
@@ -11,11 +33,11 @@ void setup() {
 }
 
 void loop() {
-    // Turn off the display:
-    lcd.noDisplay();
-    delay(500);
-    // Turn on the display:
-    lcd.display();
-    delay(500);
+    char key = keypad.getKey();
+  
+    if (key){
+      Serial.println(key);
+      lcd.print(key);
+    }
 }
 
