18e48adf8c71902afed6226f967b214887aab738
George Dewar <george@dewar.co.nz>
2014-07-26 14:44:30 +1200

    Wifi

--- lock/lock.ino
+++ lock/lock.ino
@@ -1,6 +1,11 @@
 #include <Wire.h>
 #include "rgb_lcd.h"
 #include <Keypad.h>
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+#include "Debug.h"
+#include <WiFly.h>
+#include "WiflyHTTPClient.h"
 
 /*
  * Hardware configuration
@@ -18,12 +23,24 @@ byte colPins[COLS] = {8, 7, 6}; //connect to the column pinouts of the keypad
 
 const int DOOR_STRIKE_PIN   = 2;
 const int PIR_PIN           = 3;
+const int WIFLY_RX_PIN      = 4;
+const int WIFLY_TX_PIN      = 5;
+
+#define SSID      "Door-A-Com"
+#define KEY       "qwerty0987"
+#define AUTH      WIFLY_AUTH_WPA2_PSK
+
+#define HTTP_GET_URL "http://172.26.75.139:3000/messages/poll"
+#define HTTP_POST_URL "http://172.26.75.139:3000/messages/confirm"
 
 /*
  * Set up libraries
  */
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 rgb_lcd lcd;
+SoftwareSerial wifly_uart(WIFLY_RX_PIN, WIFLY_TX_PIN);
+WiFly wifly(wifly_uart);
+HTTPClient http;
 
 /*
  * Static data
@@ -45,15 +62,36 @@ int pinChar = 0;          // The digit of the PIN that the user is up to
 char bt_buffer[BT_MAX_BYTES]; // The buffer used to store a received message
 
 void setup() {
-    Serial.begin(57600);
+    Serial.begin(9600);
+    wifly_uart.begin(9600);
   
     pinMode(DOOR_STRIKE_PIN, OUTPUT);
     pinMode(PIR_PIN, INPUT);
     
     lcd.begin(16, 2);     // set up the LCD's number of columns and rows:
     lcd.setColorAll();    // Turn backlight off
-       
+    
+    joinNetwork();   
     enterPinEntryMode();
+    
+}
+
+void joinNetwork(){
+  if (!wifly.isAssociated(SSID)) {
+    Serial.println("Connecting");
+    while (!wifly.join(SSID, KEY, AUTH)) {
+      Serial.println("Failed to join " SSID);
+      Serial.println("Wait 0.1 second and try again...");
+      delay(100);
+    }
+    Serial.println("WiFi Connected");
+    
+    wifly.save();    // save configuration, 
+  }
+}
+
+void leaveNetwork(){
+  wifly.leave(); 
 }
 
 void clearAndPrint(String text){
@@ -105,12 +143,22 @@ void checkForKey(){
 
 void loop() {
     checkForKey();
-    if(Serial.available() > 0){
-      // We have a message
-      int len = Serial.readBytesUntil(BT_END_CHAR, bt_buffer, BT_MAX_BYTES);
-      // Get ID, message
-      char id[2] = {bt_buffer[0], bt_buffer[1]};
-      
+    
+    char get;
+    Serial.println("\r\nTry to get url - " HTTP_GET_URL);
+    Serial.println("------------------------------");
+    while (http.get(HTTP_GET_URL, 10000) < 0) {
+    }
+    while (wifly.receive((uint8_t *)&get, 1, 1000) == 1) {
+      Serial.print(get);
+    }
+  
+    Serial.println("\r\n\r\nTry to post data to url - " HTTP_POST_URL);
+    Serial.println("-------------------------------");
+    while (http.post(HTTP_POST_URL, new char[3] {'a','b','c'}, 10000) < 0) {
+    }
+    while (wifly.receive((uint8_t *)&get, 1, 1000) == 1) {
+      Serial.print(get);
     }
 }
 
