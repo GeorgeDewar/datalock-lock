bd5b2f958f8816c23f7070a62076abcf5163530d
George Dewar <george@dewar.co.nz>
2014-07-26 16:46:46 +1200

    Make HTTP requests

--- lock/lock.ino
+++ lock/lock.ino
@@ -52,6 +52,8 @@ const char    CORRECT_PIN[] = {'1','2','3','4'};
 const char    BT_END_CHAR   = '\n';
 const int     BT_MAX_BYTES  = 32;
 
+const int     HTTP_MAX_BYTES = 32;
+
 /*
  * Begin program
  */
@@ -59,7 +61,8 @@ const int     BT_MAX_BYTES  = 32;
 char pin[4];              // The digits of the PIN the user is currently typing
 int pinChar = 0;          // The digit of the PIN that the user is up to
 
-char bt_buffer[BT_MAX_BYTES]; // The buffer used to store a received message
+char bt_buffer[BT_MAX_BYTES]; // The buffer used to store a received message from a phone
+char http_buffer[HTTP_MAX_BYTES]; // The buffer used to store a received message over HTTP
 
 void setup() {
     Serial.begin(9600);
@@ -141,24 +144,84 @@ void checkForKey(){
     }
 }
 
-void loop() {
-    checkForKey();
+int getResponseCode(WiFly wifly){
+  int index = 0;
+  char c;
+  char code[3];
+  while (wifly.receive((uint8_t *)&c, 1, 1000) == 1) {
+    if(index++ < 9) continue;
+    code[index-10] = c;
+    Serial.print(c);
+    if(index > 11) break;
+  }
+  return (100 * (code[0] - '0') + 10 * (code[1] - '0') + 1 * (code[2] - '0'));
+}
+
+void getPastHeaders(WiFly wifly){
+  int newLineCount = 0;
+  char c;
+  while (wifly.receive((uint8_t *)&c, 1, 1000) == 1) {
+    if(c == '\n' || c == '\r') {
+      newLineCount++;
+    }
+    else {
+      newLineCount = 0;
+    }
+    if(newLineCount == 4) break;
+  }
+}
+
+void checkForRemoteMessage() {
     
-    char get;
     Serial.println("\r\nTry to get url - " HTTP_GET_URL);
     Serial.println("------------------------------");
-    while (http.get(HTTP_GET_URL, 10000) < 0) {
+    
+    while (http.get(HTTP_GET_URL, 2000) < 0) { // wait 
     }
-    while (wifly.receive((uint8_t *)&get, 1, 1000) == 1) {
-      Serial.print(get);
+    
+    char c;
+    
+    // Read response code
+    int responseCode = getResponseCode(wifly);
+    if(responseCode == 204){
+      Serial.println(" No content");
+      return;
+    }
+    else if(responseCode != 200){
+      Serial.println(" Error" + responseCode);
+      return;
+    }
+    else {
+      Serial.println(" OK"); 
+    }
+    
+    // Get past headers
+    getPastHeaders(wifly);
+    
+    // Read actual content
+    int index = 0;
+    while (wifly.receive((uint8_t *)&c, 1, 1000) == 1) {
+      //Serial.print(c);
+      http_buffer[index++] = c;
     }
   
-    Serial.println("\r\n\r\nTry to post data to url - " HTTP_POST_URL);
-    Serial.println("-------------------------------");
-    while (http.post(HTTP_POST_URL, new char[3] {'a','b','c'}, 10000) < 0) {
-    }
-    while (wifly.receive((uint8_t *)&get, 1, 1000) == 1) {
-      Serial.print(get);
-    }
+    http_buffer[index] = 0;
+    String response = String(http_buffer);
+
+    Serial.println(response);
+  
+    //Serial.println("\r\n\r\nTry to post data to url - " HTTP_POST_URL);
+    //Serial.println("-------------------------------");
+    //while (http.post(HTTP_POST_URL, new char[3] {'a','b','c'}, 10000) < 0) {
+    //}
+    //while (wifly.receive((uint8_t *)&c, 1, 1000) == 1) {
+      //Serial.print(get);
+    //}
+}
+
+void loop() {
+    checkForKey();
+    checkForRemoteMessage();
+    
 }
 
